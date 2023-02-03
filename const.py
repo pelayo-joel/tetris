b6344ccdc3dc5fd0d31d9f0e66127e24674619c2
pelayo-joel <joel.pelayo@laplateforme.io>
2023-02-03 19:10:18 +0100

    [Update] Working on Tetromino movement and rotation (while considering wall kicks), still not displaying anything other than the playfield

--- const.py
+++ const.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 vector = pygame.math.Vector2
 
@@ -9,6 +10,7 @@ PLAYFIELD_BG = 0
 
 AUDIO_PATH = ""
 IMAGE_PATH = "images-src/"
+TILE_PATH = "images-src/TetrminoBlock/"
 FONT_PATH = ""
 
 
@@ -19,13 +21,28 @@ PLAYFIELD_COLUMNS, PLAYFIELD_ROWS = 10, 20
 PLAYFIELD_RES = PLAYFIELD_W, PLAYFIELD_H = PLAYFIELD_COLUMNS * PLAYFIELD_CELL_SIZE, PLAYFIELD_ROWS * PLAYFIELD_CELL_SIZE
 SIDE_PANEL_RES = DISPLAY_W - PLAYFIELD_W - 20, DISPLAY_H - PLAYFIELD_H - 20
 
+DIRECTIONS = {"down":vector(0, 1), "left":vector(-1, 0), "right":vector(1, 0)}
+
+COLOR = {
+    "BLACK": (0, 0, 0)
+}
 
 Tetrominoes = {
-    "I":[(0, 0), (0, 1), (0, -1), (0, -2)],
-    "T":[(0, 0), (-1, -1), (0, 1), (1, 0)],
-    "J":[(0, 0), (0, 1), (0, -1), (-1, -1)],
-    "L":[(0, 0), (0, 1), (0, -1), (1, -1)],
-    "S":[(0, 0), (0, 1), (-1, 1), (1, 0)],
-    "Z":[(0, 0), (-1, 0), (0, 1), (1, 1)],
-    "O":[(0, 0), (0, 1), (1, 0), (1, 1)],
+    "I":[(0, 0), (-1, 0), (1, 0), (2, 0)],
+    "T":[(0, 0), (-1, 0), (0, -1), (1, 0)],
+    "J":[(0, 0), (-1, 0), (1, 0), (-1, -1)],
+    "L":[(0, 0), (-1, 0), (1, 0), (1, -1)],
+    "S":[(0, 0), (-1, 0), (0, -1), (1, -1)],
+    "Z":[(0, 0), (-1, -1), (0, -1), (1, 0)],
+    "O":[(0, 0), (0, -1), (1, 0), (1, -1)],
+}
+
+RotationState = ["0", "R", "2", "L"]
+WallKickOffset = {
+    "I":[(0, 0), (0, -1), (0, 1), (0, 2)],
+    "T":[(0, 0), (-1, 0), (0, 1), (1, 0)],
+    "J":[(0, 0), (0, -1), (0, 1), (-1, -1)],
+    "L":[(0, 0), (0, -1), (0, 1), (1, 1)],
+    "S":[(0, 0), (1, 0), (0, 1), (-1, 1)],
+    "Z":[(0, 0), (-1, 0), (0, 1), (1, 1)]
 }
--- tetrisObj.py
+++ tetrisObj.py
@@ -1,11 +1,87 @@
 from const import *
-import widgets
 
-class Block:
-    def __init__(self):
-        return None
+class Tile:
+    def __init__(self, pos:pygame.Vector2, sprite:pygame.image):
+        self.pos = pos
+        self.sprite = sprite
+
+
+
+
+    def TileRotation(self, pivot:pygame.Vector2, clockOrientation):
+        degree = 90
+        if clockOrientation == "CounterClockwise":
+            degree = -90
+        origin = self.pos - pivot
+        rotated = origin.rotate(degree)
+        return rotated + pivot
+    
+    def TileCollision(self):
+        if 0 <= self.pos.x <= PLAYFIELD_W and self.pos.y <= PLAYFIELD_H:
+            return False
+        return True
+
+
+
 
 
 class Tetromino:
-    def __init__(self):
-        return None
+    def __init__(self, parent:pygame.Surface):
+        self.frame = parent
+        self.shape = random.choice(list(Tetrominoes.keys()))
+        self.tileSPrite = pygame.image.load(f"{TILE_PATH}Block{self.shape}.png")
+        self.tiles = self.__Tetromino()
+        self.rStateSelector = 0
+        self.spawnOrientation = RotationState[self.rStateSelector]
+
+
+    """Private methods"""
+
+    #Creates the tetromino itself
+    def __Tetromino(self):
+        TetrominoShape = []
+        self.tileSprite = pygame.transform.smoothscale(self.frame, (PLAYFIELD_CELL_SIZE, PLAYFIELD_CELL_SIZE))
+        
+        for pos in Tetrominoes[self.shape]:
+            tile = Tile(pos, self.tileSprite)
+            TetrominoShape.append(tile)
+        
+        return TetrominoShape
+    
+
+    """Public methods"""
+
+    #Defines the Tetromino movement
+    def Move(self, direction:pygame.Vector2):
+        nextPos = []
+        for tile in self.tiles:
+            nextPos.append(tile.pos + direction)
+
+        if not self.Colliding(nextPos):
+            for tile in self.tiles:
+                tile.pos += direction
+
+    def Rotate(self, clockOrientation):
+        nextPos = []
+        for tile in self.tiles:
+            nextPos.append(tile.TileRotation(self.tiles[0].pos, clockOrientation))
+        
+        if not self.Colliding(nextPos):
+            for tile in self.tiles:
+                tile.TileRotation(self.tiles[0], clockOrientation)
+            if clockOrientation == "Clockwise":
+                self.rStateSelector += 1
+            elif clockOrientation == "CounterClockwise":
+                self.rStateSelector -= 1
+
+        elif clockOrientation == "Clockwise":
+            return None
+        
+        self.spawnOrientation = RotationState[self.rStateSelector]
+
+            
+
+    #Checks if pos is colliding
+    def Colliding(self, posToCheck):
+        return any(map(Tile.TileCollision, self.tiles, posToCheck))
+            
